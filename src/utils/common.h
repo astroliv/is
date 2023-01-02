45f74c6e94fdc28f3b6510f6c1ebcae0afa8e0ec
invefa <invefa@outlook.com>
2023-01-02 15:23:40 +0800

    [common.h]assert():msg改为支持c-fmt格式

--- src/utils/common.h
+++ src/utils/common.h
@@ -6,7 +6,7 @@
 //将输入数字向上对齐到2的整次幂
 uint32_t ceilToPowerOf2(uint32_t num);
 
-//断言,即判断logicExpr是否为false,若为false则弹出msg并退出程序,否则不进行操作
+//断言,即判断logicExpr是否为false,若为false则弹出错误信息msg(msg支持c-fmt)并退出程序,否则不进行操作
 void assert(bool logicExpr, const char *msg, ...);
 
 #endif //IS_COMMON_H
